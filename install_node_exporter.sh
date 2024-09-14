028fdc2118f13ab1f465d5b65e98d2d39528bebc
Andrei <aformusatii@gmail.com>
2024-09-14 22:09:11 +0300

    first commit

---
+++ install_node_exporter.sh
@@ -0,0 +1,8 @@
+# Download binary of node exported from
+# https://prometheus.io/download/#node_exporter
+# https://github.com/prometheus/node_exporter/releases
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+cd node_exporter-*.*-amd64
+
